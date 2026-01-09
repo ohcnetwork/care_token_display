0bfbe065ea7635fd86a26932f43f9a0016ded63d
rithviknishad <mail@rithviknishad.dev>
2026-01-09 15:56:06 +0530

    Revert "Added authentication"
    This reverts commit 2d1857d7ebb199f4ffd3353db48e79ea74920302.

--- src/token_display/views.py
+++ src/token_display/views.py
@@ -4,6 +4,12 @@ from django.core.cache import cache
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
+from token_display.settings import plugin_settings
+from token_display.utils import (
+    fmt_schedule_resource_name,
+    fmt_token_number,
+    get_token_display_cache_key,
+)
 
 from care.emr.models import Token, TokenSubQueue
 from care.emr.resources.scheduling.token.spec import TokenStatusOptions
@@ -12,18 +18,6 @@ from care.emr.resources.scheduling.token_sub_queue.spec import (
 )
 from care.utils.shortcuts import get_object_or_404
 from care.utils.time_util import care_now
-from token_display.settings import plugin_settings
-from token_display.utils import (
-    fmt_schedule_resource_name,
-    fmt_token_number,
-    get_token_display_cache_key,
-)
-
-
-def authenticate_request(request: HttpRequest) -> bool:
-    from rest_framework.authtoken.models import Token as AuthToken
-
-    get_object_or_404(AuthToken, key=request.GET.get("token"))
 
 
 class SubQueuesTokenDisplayView(View):
@@ -35,7 +29,6 @@ class SubQueuesTokenDisplayView(View):
         """
         Render the full token display page with HTMX setup.
         """
-        authenticate_request(request)
         sub_queues = TokenSubQueue.objects.filter(
             external_id__in=sub_queue_external_ids.split(","),
             status=TokenSubQueueStatusOptions.active.value,
@@ -79,7 +72,6 @@ class SubQueuesTokenDisplayView(View):
             "item_count": item_count,
             "grid_class": grid_class,
             "refresh_interval": plugin_settings.TOKEN_DISPLAY_REFRESH_INTERVAL,
-            "auth_token": request.GET.get("token"),
         }
         return render(request, "token_display/display.html", context)
 
@@ -97,7 +89,6 @@ class SubQueueTokenDisplayPartialView(View):
         """
         Return the partial HTML with updated token data for a specific sub queue.
         """
-        authenticate_request(request)
         cache_key = get_token_display_cache_key(sub_queue_external_id)
         cached_html = cache.get(cache_key)
         if cached_html is not None:
